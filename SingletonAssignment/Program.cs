cc08c721c049c246e059dd399664db655839c06f
jakjon <jakejones506@gmail.com>
2020-02-17 21:26:20 -0500

    project completed

--- SingletonAssignment/Program.cs
+++ SingletonAssignment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Timers;
+using SingletonAssignment;
 
 namespace SingletonAssignment
 {
@@ -6,7 +8,49 @@ namespace SingletonAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            SingletonTimer timer = SingletonTimer.GetSingletonTimer();
+            timer.startTimer();
+
+            start();
+
+            void start() {
+
+                string time;
+                string input;
+               
+                Console.WriteLine("\n A singleton timer has been created...");
+                Console.WriteLine("\n Options: \n\n I: \t show running time in milliseconds \n S: \t show running time in seconds \n M: \t show running time in minutes \n H: \t show running time in hours \n\n");
+
+                input = Console.ReadLine().ToUpper();
+                
+                switch (input)
+                {
+                    case "I":
+                        time = timer.getRunningTime();
+                        Console.WriteLine("\n\n\t RUN TIME: " + time + " Milliseconds");
+                        start();
+                        break;
+                    case "S":
+                        time = timer.getRunningTimeInSeconds();
+                        Console.WriteLine("\n\n\t RUN TIME: " + time + " Seconds");
+                        start();
+                        break;
+                    case "M":
+                        time = timer.getRunningTimeInMinutes();
+                        Console.WriteLine("\n\n\t RUN TIME: " + time + " Minutes");
+                        start();
+                        break;
+                    case "H":
+                        time = timer.getRunningTimeInHours();
+                        Console.WriteLine("\n\n\t RUN TIME: " + time + " Hours");
+                        start();
+                        break;
+                    default:
+                        start();
+                        break;
+                }           
+            }
         }
     }
 }
