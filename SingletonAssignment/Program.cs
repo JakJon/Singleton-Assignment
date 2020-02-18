815bc5be9db5238b9c2ceeab2761c879287e06ad
jakjon <jakejones506@gmail.com>
2020-02-17 19:37:42 -0500

    tracking files

---
+++ SingletonAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingletonAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
